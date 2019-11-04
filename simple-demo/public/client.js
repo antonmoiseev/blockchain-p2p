25a39bb06e11c9a80b35588c57e6b4511b468b3c
Anton Moiseev <a@antonmoiseev.com>
2019-11-04 18:07:09 +0200

    Clean up establishing connection demo

--- simple-demo/public/client.js
+++ simple-demo/public/client.js
@@ -1,111 +1,97 @@
-const ws = new WebSocket('ws://localhost:8080');
-let rtcConnection = null;
-let dataChannel = null;
-
+document.getElementById('connect').addEventListener('click', sendOffer);
 document.getElementById('send').addEventListener('click', () => {
-    dataChannel.send('Yeeeeeessss!!!! IT WORKS!');
+  dataChannel.send('Current time: ' + new Date().toLocaleTimeString());
 });
 
-document.getElementById('connect').addEventListener('click', () => {
-    console.log('Generating an offer, setting the local description');
+// Signaling server
+const ws = new WebSocket('ws://localhost:8080');
+ws.addEventListener('message', handleMessageFromSignalingServer);
 
-    dataChannel = rtcConnection.createDataChannel('sendChannel');
 
-    rtcConnection.createOffer()
-        .then(offer => rtcConnection.setLocalDescription(offer))
-        .then(() => ws.send(JSON.stringify({
-            type: 'offer',
-            data: rtcConnection.localDescription
-        }, null, 2)));
-    
-    dataChannel.onopen = () => {
-        console.log('onopen');
-    };
+// Setup WebRTC RTCPeerConnection
+let dataChannel = null;
+let rtcConnection = new RTCPeerConnection();
+rtcConnection.ondatachannel = (e) => {
+  log('[WebRTC] Received data channel.');
+  dataChannel = e.channel;
+  setupDataChannel(dataChannel);
+};
 
-    dataChannel.onclose = (e) => {
-        console.log('onclose', e);
-    };
+rtcConnection.onicecandidate = (e) => {
+  log('[WebRTC] Sending an ICE candidate...', '#666');
+  if (e.candidate) {
+    ws.send(JSON.stringify({ type: 'add-ice-candidate', data: e.candidate }));
+  }
+};
 
-    dataChannel.onmessage = (m) => {
-        console.log('onmessage', m);
-    };
-});
+function sendOffer() {
+  log('[WebRTC] Creating a data channel...');
+  dataChannel = rtcConnection.createDataChannel('DEMO DATA CHANNEL');
+  setupDataChannel(dataChannel);
 
-ws.addEventListener('open', () => {
-    console.log('connection opened');
-    
-    rtcConnection = new RTCPeerConnection();
+  log('[WebRTC] Creating an offer...');
+  rtcConnection.createOffer()
+    .then(offer => {
+      log('[WebRTC] Sending the offer...');
+      rtcConnection.setLocalDescription(offer);
+    })
+    .then(() => {
+      log('[WebRTC] Setting local session description..');
+      ws.send(JSON.stringify({ type: 'offer', data: rtcConnection.localDescription }));
+    });
+}
 
-    // let negotiating = false;
-    // rtcConnection.onnegotiationneeded = async e => {
-    //     try {
-    //         if (negotiating || rtcConnection.signalingState != "stable") return;
-    //         negotiating = true;
-    //         /* Your async/await-using code goes here */
-    //     } finally {
-    //         negotiating = false;
-    //     }
-    // }
-
-    rtcConnection.ondatachannel = (e) => {
-        console.log('ondatachannel', e);
-        if (!dataChannel) dataChannel = e.channel;
-    };
-
-    rtcConnection.onicecandidate = (e) => {
-        console.log('onicecandidate');
-        if (e.candidate) {
-            ws.send(JSON.stringify({
-                type: 'add-ice-candidate',
-                data: e.candidate
-            }, null, 2));
-        }
-    };
-
-    // if (new Date().getSeconds() < 30) {
-        
-        // }
-})
-
-ws.addEventListener('closed', () => {
-    console.log('connection opened');
-})
-
-ws.addEventListener('message', (e) => {
-    // console.log('Message:', e.data);
-    const message = JSON.parse(e.data);
-    
-    if (message.type === 'add-ice-candidate') {
-        rtcConnection.addIceCandidate(message.data)
-            .catch(() => console.log('Cannot add ICE candidate:', message.data));
-    } else if (message.type === 'offer') {
-        console.log('Received an offer, setting the remote description');
-        if (!rtcConnection.remoteDescription) {
-        rtcConnection.setRemoteDescription(message.data)
-            .then(() => rtcConnection.createAnswer())
-            .then(answer => {
-                if (!rtcConnection.localDescription) {
-                    console.log('Setting local description from answer');
-                    rtcConnection.setLocalDescription(answer)
-                } else {
-                    console.log('Already have a description, ignoring answer');
-                }
-                return answer;
-            })
-            .then(answer => ws.send(JSON.stringify({
-                type: 'answer',
-                data: answer
-            }, null, 2)));
-        }
-    } else if (message.type === 'answer') {
-        console.log('Received an answer, setting the remote description');
-        if (!rtcConnection.remoteDescription) {
-            console.log('Setting remote description from answer');
-            rtcConnection.setRemoteDescription(message.data);
-        } else {
-            console.log('Already have a description, ignoring answer');
-        }
-    } else {
-        console.log('Unknown message type');
+function handleMessageFromSignalingServer(messageEvent) {
+  const message = JSON.parse(messageEvent.data);
+  
+  switch (message.type) {
+    case 'add-ice-candidate': {
+      rtcConnection
+        .addIceCandidate(message.data)
+        .catch(() => log('[WebRTC] Cannot add ICE candidate: ' + message.data, 'red'));
+      break;
     }
-})
+  
+    case 'offer': {
+      log('[WebRTC] Received an offer.');
+      if (!rtcConnection.remoteDescription) {
+        log('[WebRTC] Setting up remote session description.');
+        rtcConnection.setRemoteDescription(message.data)
+          .then(() => {
+            log('[WebRTC] Creating an answer...');
+            return rtcConnection.createAnswer();
+          })
+          .then(answer => {
+            log('[WebRTC] Setting local description...');
+            rtcConnection.setLocalDescription(answer);
+            return answer;
+          })
+          .then(answer => {
+            log('[WebRTC] Sending the answer...');
+            ws.send(JSON.stringify({ type: 'answer', data: answer }));
+          });
+      }
+      break;
+    }
+  
+    case 'answer': {
+      log('[WebRTC] Received an answer');
+      log('[WebRTC] Setting remote session description...');
+      rtcConnection.setRemoteDescription(message.data);
+      break;
+    }
+  
+    default: {
+      log('[WebRTC] Unknown message type', 'red');
+    }
+  }
+}
+
+function setupDataChannel(dataChannel) {
+  dataChannel.onopen = () => log('[WebRTC] Data channel opened.');
+  dataChannel.onmessage = (messageEvent) => log(`[WebRTC] ${messageEvent.data}`, 'green');
+}
+
+function log(message, color = '#000') {
+  console.log(`%c${message}`, `color: ${color}`);
+}
